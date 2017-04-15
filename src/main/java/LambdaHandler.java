e04bdd63c30d7968e1697ee2ad45e500e5f06dc7
Sergei Mikhailov <sergei@mikhailov.nz>
2017-04-16 09:51:30 +1200

    Should implement AWS Lambda API

---
+++ src/main/java/LambdaHandler.java
@@ -0,0 +1,13 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler
+    implements RequestHandler<Void, String> {
+
+  @Override
+  public String handleRequest(Void input, Context context) {
+
+    return "";
+  }
+
+}
---
+++ src/test/java/LambdaHandlerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LambdaHandlerTest {
+
+  private LambdaHandler handler = new LambdaHandler();
+
+  @Test
+  public void testSomeLibraryMethod() {
+
+    String result = handler.handleRequest(null, null);
+    assertThat(result).isEmpty();
+  }
+
+}
